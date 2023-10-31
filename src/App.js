daebf86383fc179da478e36f1f9c2091db4fb2ba
aashish_k <aashishkathat00@gmail.com>
2023-10-31 23:39:22 +0530

    handling events

--- src/App.js
+++ src/App.js
@@ -7,26 +7,40 @@ const messages = [
 export default function App() {
 	const step = 1;
 
-	return (
-		<div className="steps">
-			<div className="numbers">
-				<div className={`${step >= 1 ? "active" : ""}`}>1</div>
-				<div className={`${step >= 1 ? "active" : ""}`}>2</div>
-				<div className={`${step >= 1 ? "active" : ""}`}>3</div>
-			</div>
+function handlePrevious() {
+	alert("puuu");
+}
+function handleNext() {
+	alert("next");
+}
 
-			<p className="message">
-				Setp {step}: {messages[step - 1]}
-			</p>
-
-			<div className="buttons">
-				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-					Previous
-				</button>
-				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-					Next
-				</button>
-			</div>
+return (
+	<div className="steps">
+		<div className="numbers">
+			<div className={`${step >= 1 ? "active" : ""}`}>1</div>
+			<div className={`${step >= 1 ? "active" : ""}`}>2</div>
+			<div className={`${step >= 1 ? "active" : ""}`}>3</div>
 		</div>
-	);
+
+		<p className="message">
+			Setp {step}: {messages[step - 1]}
+		</p>
+
+		<div className="buttons">
+			<button
+				style={{ backgroundColor: "#7950f2", color: "#fff" }}
+				onClick={() => alert("poop")}
+				onMouseEnter={handlePrevious}
+			>
+				Previous
+			</button>
+			<button
+				style={{ backgroundColor: "#7950f2", color: "#fff" }}
+				onMouseEnter={handleNext}
+			>
+				Next
+			</button>
+		</div>
+	</div>
+);
 }
